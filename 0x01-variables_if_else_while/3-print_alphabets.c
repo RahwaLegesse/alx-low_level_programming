17947f98d08206b58d6ea1788f5cd7c7c6d8cf6a
RahwaLegesse <RahwaLegesse31@gmail.com>
2023-06-16 13:10:31 -0700

    no

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+lowA;
+for (lowA = 'a' ; lowA <= 'z' ; lowA++)
+putchar(low);
+for (lowA = 'A' ; lowA <= 'Z' ; lowA++)
+putchar(low);
+putchar('\n');
+return (0);
+}
+
