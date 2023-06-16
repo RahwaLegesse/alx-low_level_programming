727bcce6710afe2068918fe4cfbbaf086149cd30
RahwaLegesse <RahwaLegesse31@gmail.com>
2023-06-16 03:32:29 -0700

    No7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	char a;
 	int b;
@@ -7,11 +12,11 @@ int main()
 	long long int d;
 	float e;
 	//sizeof the variable
-	printf("Size of int: %zu bytes\n", sizeof(a));
-        printf("Size of float: %zu bytes\n", sizeof(b));
-        printf("Size of double: %zu bytes\n", sizeof(c));
-        printf("Size of char: %zu byte\n", sizeof(d));
-	printf("Size of char: %zu byte\n", sizeof(e));
+	printf("Size of char: %zu bytes\n",(unsigned long)sizeof(a));
+        printf("Size of int: %zu bytes\n",(unsigned long)sizeof(b));
+        printf("Size of long int: %zu bytes\n",(unsigned long)sizeof(c));
+        printf("Size of long long int: %zu byte\n",(unsigned long)sizeof(d));
+	printf("Size of float: %zu byte\n",(unsigned long)sizeof(e));
 
 	return (0);
 }	
