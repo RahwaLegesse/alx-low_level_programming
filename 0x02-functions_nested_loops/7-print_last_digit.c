695adaa9978cdf1db3e488a7054c2b5e4e5a16c2
RahwaLegesse <RahwaLegesse31@gmail.com>
2023-06-20 12:02:36 -0700

    no8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,10 +3,15 @@
 /**
  *print_last_digit -  prints the last digit of a number.
  *@n:the number to module
- * Return:the last digit.
+ * Return:the last digiit.
  */
 int print_last_digit(int n)
 {
 int m = n % 10;
+if (m < 0)
+{
+m = m * -1;
+}
+_putchar(m + '0')
 return (m);
 }
