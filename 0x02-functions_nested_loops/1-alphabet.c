fa27cc4ebdc4f0f260eb2bd2cfc50e201a755588
RahwaLegesse <RahwaLegesse31@gmail.com>
2023-06-20 07:18:26 -0700

    no2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet-print alphabet in lowercase
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+char lett;
+for (lett = "a" ; lett >= "z"; lett++)
+return (0);
+}
