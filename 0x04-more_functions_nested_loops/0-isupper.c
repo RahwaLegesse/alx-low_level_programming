3425abf4b5de6a58ab8f1f61dc3c7a9d8209207e
RahwaLegesse <RahwaLegesse31@gmail.com>
2023-06-22 02:40:03 -0700

    n01

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,17 @@
 
 /**
  * _isupper - checks for uppercase character
- *@c:the number to be checked
- * Return:1 if uper 0 if anything
+ * @c:to be checked
+ * Return:1 if upper 0 if anything
  */
 int _isupper(int c)
-{      
-if (c >= 65 && c <= 90)
 {
-return (1);
-}
-return (0);
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
