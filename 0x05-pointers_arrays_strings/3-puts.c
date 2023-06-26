2c9b778143bde3fbb3caaaf87ec6eeeb34a5bbdb
RahwaLegesse <RahwaLegesse31@gmail.com>
2023-06-26 10:10:25 -0700

    No3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - followed by a new line, to stdout
+ * @str:to dispaly string
+ * Return: str
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
