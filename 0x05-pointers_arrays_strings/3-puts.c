0c3adfc74ef28d394494e4bc2d6ba58ffd436568
RahwaLegesse <RahwaLegesse31@gmail.com>
2023-06-26 10:12:41 -0700

    No3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,16 +2,16 @@
 
 /**
  * _puts - followed by a new line, to stdout
- * @str:to dispaly string
+ * @s:to dispaly string
  * Return: str
  */
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
-		_putchar(str[i]);
+		_putchar(s[i]);
 		i++;
 	}
 }
