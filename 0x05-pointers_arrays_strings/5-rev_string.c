36b9abd4582e9d11eb8e8e0da982a9f7dd3ba477
RahwaLegesse <RahwaLegesse31@gmail.com>
2023-06-26 11:13:22 -0700

    No5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,18 +7,18 @@
  */
 void rev_string(char *s)
 {
-        int i = 0;
-        int j;
-        int temp;
+int i = 0;
+int j;
+int temp;
 
-        while (s[i] != '\0')
-        {
-                i++;
-        }
-        for (j = i - 1; j >= i / 2; j--)
-        {
-                temp = s[j];
-                s[j] = s[i - j - 1];
-                s[i - j - 1] = temp;
-        }
+while (s[i] != '\0')
+{
+i++;
+}
+for (j = i - 1; j >= i / 2; j--)
+{
+temp = s[j];
+s[j] = s[i - j - 1]
+s[i - j - 1] = temp;
+}
 }
