d2e263b28f2080c1e354d7948dd000848e5af03c
RahwaLegesse <RahwaLegesse31@gmail.com>
2023-06-29 06:38:20 -0700

    no5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - changes all lowercase letters
+ * @a:the number to change
+ * Return: a
+ */
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 97 && a[i] <= 122)
+			a[i] = a[i] - 32;
+		i++;
+	}
+	return (a);
+}
