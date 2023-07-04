492881104ff08cb9f6a3ab705f8892f0d429b022
RahwaLegesse <RahwaLegesse31@gmail.com>
2023-07-03 23:39:03 -0700

    no5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,7 +16,7 @@ char *_strstr(char *haystack, char *needle)
 	while (*haystack)
 		i = 0;
 	{
-		if (needle[i] == haystack[i])
+		if (haystack[i] == needle[i])
 		{
 			do {
 				if (needle[i + 1] == '\0')
