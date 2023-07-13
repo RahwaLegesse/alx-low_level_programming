56e5327e8e5ecdf319a0188edfed38abc24b321f
RahwaLegesse <rahwalegesse31@.com>
2023-07-13 13:26:48 -0700

    no3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,18 +13,18 @@
  */
 int *array_range(int min, int max)
 {
-        int *ptr;
-        int i, k = 0;
+int *ptr;
+int i, k = 0;
 
-        if (min > max)
-                return (NULL);
+if (min > max)
+return (NULL);
 ptr = malloc(sizeof(int) * (max - min + 1));
 if (ptr == NULL)
 return (NULL);
 for (i = min; i <= max; i++)
 {
-        *(ptr + k) = i;
-        k++;
+*(ptr + k) = i;
+k++;
 }
 return (ptr);
 }
