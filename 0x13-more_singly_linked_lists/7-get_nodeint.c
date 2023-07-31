64a5058a995dcd6f90e38b48c6fb9c956684cb94
RahwaLegesse <RahwaLegesse31@gmail.com>
2023-07-31 13:43:00 -0700

    no7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,9 +4,23 @@
 #include "lists.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * get_nodeint_at_index - the nth node of a listint_t
+ * @head:the first pointor
+ * @index:the number we want
+ * Return: new or null.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	unsigned int i = 0;
+	listint_t *new = head;
+
+	while (new && i < index)
+	{
+		new = new->next;
+		i++;
+	}
+	if (new)
+		return (new);
+	else
+		return (NULL);
+}
