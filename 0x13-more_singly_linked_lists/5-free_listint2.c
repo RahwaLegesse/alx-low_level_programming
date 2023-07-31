33294b3c90e1f568b8aaa586feb50be554af9cba
RahwaLegesse <RahwaLegesse31@gmail.com>
2023-07-31 11:18:14 -0700

    no5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * free_listint2 -  uses to free
+ * @head: pointer to the listint_t 
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *new;
+
+	if (head == NULL)
+		return;
+
+	while (*head)
+	{
+		new = (*head)->next;
+		free(*head);
+		*head = new;
+	}
+}
