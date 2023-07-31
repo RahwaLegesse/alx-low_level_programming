6152be839c98f2358866db10a57aacb5220d8cb6
RahwaLegesse <RahwaLegesse31@gmail.com>
2023-07-31 13:29:22 -0700

    no0

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
