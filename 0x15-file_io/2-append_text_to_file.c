05daa9526c8ad5c309c8dd2f490aec059a5a94f6
RahwaLegesse <RahwaLegesse31@gmail.com>
2023-08-09 06:06:17 -0700

    no2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * append_text_to_file - add the new text to the old one
+ * @filename:pointer to the file
+ * @text_content:the content of the file
+ * Return: 1 if it passes -1 if failed
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int p, wt;
+	int length = 0;
+
+	if (filename == 0)
+	return (-1);
+	if (text_content == 0)
+	{
+		for (length = 0; text_content[length]; )
+			length++;
+	}
+	p = open(filename, O_WRONLY | O_APPEND);
+	wt = write(p, text_content, length);
+	if (p == -1)
+		return (-1);
+	if (wt == -1)
+		return (-1);
+	close(p);
+	return (1);
+}
