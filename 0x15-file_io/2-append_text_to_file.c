7b6cdf3b9eccc3c0c84375738dd55b60d7227058
RahwaLegesse <RahwaLegesse31@gmail.com>
2023-08-09 11:57:27 -0700

    no2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,11 +13,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	int p, wt;
 	int length = 0;
 
-	if (filename == 0)
+	if (filename == NULL)
 	return (-1);
-	if (text_content == 0)
+	if (text_content != NULL)
 	{
-		for (length = 0; text_content[length]; )
+		for (length = 0; text_content[length];)
 			length++;
 	}
 	p = open(filename, O_WRONLY | O_APPEND);
