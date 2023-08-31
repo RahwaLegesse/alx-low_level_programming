cbd14efac4af453ff507f9dcbd92ddd1b4985a05
rahwalegesse <rahwalegesse31@gmail.com>
2023-08-31 02:49:51 -0700

    number 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n:integer
+ * @index:index
+ * Return: dec.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int dec = 0;
+
+	if (index > 63)
+		return (0);
+	dec = (n >> index) & 1;
+	return (dec);
+}
